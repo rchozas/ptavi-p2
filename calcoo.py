2f5f7f18df49b27df01a00afbd0938e37c3b25a2
Raquel Chozas <r.chozas@alumnos.urjc.es>
2018-09-26 20:05:22 +0200

    calcoo

--- calcoo.py
+++ calcoo.py
@@ -1,3 +1,40 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
+
+
+class Calculadora():
+
+    def __init__(self,op1,op2):
+
+        self.op1= operando1
+        self.op2 = operando2
+
+    def suma(self):
+       
+        return self.op1 + self.op2
+
+
+    def resta(self):
+        
+        return self.op1 - self.op2
+
+if __name__ == "__main__":
+    try:
+        operando1 = float(sys.argv[1])
+        operacion = sys.argv[2]
+        operando2 = float(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    calculos = Calculadora(operando1, operando2)
+
+    if operacion == "suma":
+        resultado = calculos.suma()
+    elif operacion == "resta":
+        resultado = calculos.resta()
+    else:
+        sys.exit("Operación sólo puede ser suma o resta.")
+
+    print(resultado)
