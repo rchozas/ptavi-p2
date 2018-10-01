151e2a52ab7f6b847d99ef04a18548c5a392796e
Raquel Chozas <r.chozas@alumnos.urjc.es>
2018-10-01 21:19:01 +0200

    pep8

--- calcplus.py
+++ calcplus.py
@@ -5,25 +5,20 @@ import sys
 import calcoohija
 
 if __name__ == '__main__':
-    
+
     calcuplus = calcoohija.CalculadoraHija(operando1, operando2)
 
     fichero = open(sys.argv[1] "r")
     lineas = fichero.readlines()
-    
-         
-        
-        if operacion == "suma":
-            resultado = calcuplus.suma()
-        elif operacion == "resta":
-            resultado = ccalcuplus.resta()
-        elif operacion == "multiplica":
-            resultado = calcuplus.multiplica()
-        elif operacion == "divide":
-            try:
-                resultado = calcuplus.divide()
-            except ZeroDivisionError:
-                sys.exit("Division by zero is not allowed") 
-
-    
+    if operacion == "suma":
+        resultado = calcuplus.suma()
+    elif operacion == "resta":
+        resultado = calcuplus.resta()
+    elif operacion == "multiplica":
+        resultado = calcuplus.multiplica()
+    elif operacion == "divide":
+        try:
+            resultado = calcuplus.divide()
+        except ZeroDivisionError:
+            sys.exit("Division by zero is not allowed")
     Fichero.close()
