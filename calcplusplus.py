b97d9630f95cf9471b8cf8133aec7f8f739f5686
Raquel Chozas <r.chozas@alumnos.urjc.es>
2018-10-01 20:43:29 +0200

    calcplusplus

--- calcplusplus.py
+++ calcplusplus.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
+import csv
+import calcoohija
+import calcplus
+
+if __name__ == "__main__":
+    
+    with open(sys.argv[1]) as fichero:
+        infor = csv.reader(fichero)
+        
+        
